beeb18be176f0c13fc465cf04abf068ad107dc19
Marcos Pablo <marcos@docker.example.com>
2020-10-10 08:56:16 -0300

    modified:   Dockerfile 	deleted:    arf.json 	renamed:    arf.json.ant -> config/arf.json 	renamed:    index.html -> config/index.html 	modified:   ejecutar.sh

--- OSINT-Framework/ejecutar.sh
+++ OSINT-Framework/ejecutar.sh
@@ -1,1 +1,3 @@
-docker run -p 5002:8000 -d osintframework:latest 
+docker run -p 5002:8000 \
+      -v $PWD/config/arf.json:/OSINT-Framework/public/arf.json:ro \
+      -v $PWD/config/index.html:/OSINT-Framework/public/index.html:ro -d osintframework:latest 
