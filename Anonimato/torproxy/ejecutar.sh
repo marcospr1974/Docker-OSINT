03a46873bf212a4b643dfdfff1677edc1c442c46
Marcos Pablo <marcos@docker.example.com>
2020-10-10 09:52:15 -0300

    modified:   ejecutar.sh

--- Anonimato/torproxy/ejecutar.sh
+++ Anonimato/torproxy/ejecutar.sh
@@ -1,2 +1,1 @@
 docker run -it -p 8118:8118 -p 9050:9050 -v $PWD/config/etc/torirc:/etc/tor/torrc:ro -v $PWD/config/tor:/var/lib/tor -d torproxy:latest
-#docker run -it -p 8118:8118 -p 9050:9050  -v $PWD/config/tor:/var/lib/tor -d torproxy:latest
