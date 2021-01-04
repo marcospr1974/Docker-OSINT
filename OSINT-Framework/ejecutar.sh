dcf4a9dd700fe443fbe5e7c5c73c118c2facb0c6
Marcos Pablo Russo <marcospr1974@gmail.com>
2021-01-04 16:13:05 -0300

    Cambios a ser confirmados: 	modificado:     OSINT-Framework/Dockerfile 	borrado:        OSINT-Framework/config/arf.json.ant 	nuevo archivo:  OSINT-Framework/config/bandera.png 	modificado:     OSINT-Framework/config/index.html 	modificado:     OSINT-Framework/ejecutar.sh

--- OSINT-Framework/ejecutar.sh
+++ OSINT-Framework/ejecutar.sh
@@ -1,3 +1,7 @@
 docker run -p 5002:8000 \
+      -d \
       -v $PWD/config/arf.json:/OSINT-Framework/public/arf.json:ro \
-      -v $PWD/config/index.html:/OSINT-Framework/public/index.html:ro -d osintframework:latest 
+      -v $PWD/config/bandera.png:/OSINT-Framework/public/static/bandera.png:ro \
+      -v $PWD/config/index.html:/OSINT-Framework/public/index.html:ro \
+      osintframework:latest
+
